2c919264e0f643936b5e8077231ad926785ce85b
AABur <aabur@mail.ru>
2021-05-12 22:12:21 +0300

    refactor: change .get() for direct get value by dict key

--- src/hexlet/fs/__init__.py
+++ src/hexlet/fs/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-'''Description'''
+"""Description."""
 
 __all__ = (   # noqa: WPS317
     'mkfile', 'mkdir',
@@ -9,47 +9,47 @@ __all__ = (   # noqa: WPS317
 
 
 def mkfile(name, meta={}):
-    '''Return file node.'''
+    """Return file node."""
     return {
-            'name': name,
-            'meta': meta,
-            'type': 'file',
-           }
+        'name': name,
+        'meta': meta,
+        'type': 'file',
+    }
 
 
 def mkdir(name, children=[], meta={}):
-    '''Return directory node.'''
+    """Return directory node."""
     return {
-            'name': name,
-            'children': children,
-            'meta': meta,
-            'type': 'directory',
-           }
+        'name': name,
+        'children': children,
+        'meta': meta,
+        'type': 'directory',
+    }
 
 
 def is_directory(node):
-    '''Check is node a directory.'''
-    return node.get('type') == 'directory'
+    """Check is node a directory."""
+    return node['type'] == 'directory'
 
 
 def is_file(node):
-    '''Check is node a file.'''
-    return node.get('type') == 'file'
+    """Check is node a file."""
+    return node['type'] == 'file'
 
 
 def get_children(directory):
-    '''Return children of node.'''
-    return directory.get('children')
+    """Return children of node."""
+    return directory.get('children', [])
 
 
 def get_meta(node):
-    '''Return meta of node.'''
-    return node.get('meta')
+    """Return meta of node."""
+    return node['meta']
 
 
 def get_name(node):
-    '''Return name of node.'''
-    return node.get('name')
+    """Return name of node."""
+    return node['name']
 
 
 def flatten(tree):
